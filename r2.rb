bf9be789ed6bb307b6f79a7976d854e64d7add65
tloring <thom@tloring.com>
2014-01-26 11:17:01 -0500

    major factoring

--- r2.rb
+++ r2.rb
@@ -5,25 +5,39 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
+Station = Struct.new(:code, :name, :minutes_to, :minutes_from, :northbound_station_index, :southbound_station_index)
+
 class SeptaR2
 
-  CODES = { :claymont => "Claymont", :thirtieth => "30th Street Station" }
+  ROUTE_CODE = "WIL"
+
+  # northbound order
+  STATIONS = [ "Newark", "Churchmans Crossing", "Wilmington", "Claymont", "Marcus Hook", "Highland Avenue", 
+               "Chester", "Eddystone", "Crum Lynne", "Ridley Park", "Prospect Park", "Norwood", "Glenolden", 
+               "Folcroft", "Sharon Hill", "Curtis Park", "Darby", "University City", "30th Street Station", 
+               "Suburban Station", "Market East Station", "Temple University"]
 
   def initialize(origin, destination)
     @origin = origin
     @destination = destination
   end
 
+  def direction
+    STATIONS.index(@origin.name) < STATIONS.index(@destination.name) ? :northbound : :southbound
+  end
+
+  def stations(direction_sym)
+    direction_sym == :northbound ? STATIONS : STATIONS.reverse
+  end
+
   def flip!
-    tmp = @origin
-    @origin = @destination
-    @destination = tmp
+    @origin, @destination = @destination, @origin
   end
 
   def next
-    output = "Next to Arrive #{CODES[@origin]} >> #{CODES[@destination]}\n\n"
+    output = "Next to Arrive #{@origin.name} >> #{@destination.name}\n\n"
 
-    url = "http://www3.septa.org/hackathon/NextToArrive/#{CODES[@origin]}/#{CODES[@destination]}/20"
+    url = "http://www3.septa.org/hackathon/NextToArrive/#{@origin.name}/#{@destination.name}/20"
     response = JSON.parse open(URI::encode(url)).read
   
     response.each do |line|
@@ -33,15 +47,14 @@ class SeptaR2
   end
 
   def schedule
-    output = "Weekday Schedule #{CODES[@origin]} >> #{CODES[@destination]}\n\n"
+    output = "Weekday Schedule #{@origin.name} >> #{@destination.name}\n\n"
 
-    route = "WIL"
-    origin_index = 4 
-    destination_index = 19
-    direction_code = (@origin == :thirtieth) ? 0 : 1  # southbound=0, northbound=1
+    direction_code = direction == :northbound ? 1 : 0
+    origin_index = stations(direction).index(@origin.name) + 1
+    destination_index = stations(direction).index(@destination.name) + 1
     train_numbers = origin_times = destination_times = nil 
 
-    url = "http://www.septa.org/schedules/rail/w/#{route}_#{direction_code}.html"
+    url = "http://www.septa.org/schedules/rail/w/#{ROUTE_CODE}_#{direction_code}.html"
     doc = Nokogiri::HTML(open(url))
 
     # there are 2 tables with ID of timeTable, so re-parse 2nd table and work on those rows
@@ -63,7 +76,17 @@ end
 
 if $0 == __FILE__
 
-  r2 = SeptaR2.new :claymont, :thirtieth
+  claymont = Station.new
+  claymont.name = "Claymont"
+  claymont.minutes_to = 20
+  claymont.minutes_from = 15
+
+  thirtieth = Station.new
+  thirtieth.name = "30th Street Station"
+  thirtieth.minutes_to = 15
+  thirtieth.minutes_from = 10
+
+  r2 = SeptaR2.new claymont, thirtieth
   puts r2.next
   puts r2.schedule
   r2.flip!
